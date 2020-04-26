74da209615aa93aed32cb6dc4442b1734555e2c1
Christopher Marti <chrisjm093@gmail.com>
2020-04-25 17:14:39 -0700

    Question and button functionality is working

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var timerEl = document.getElementById("timer");
 var secondsLeft = 75;
-var questionArea = document.getElementById("question-area");
+var questionContainer = document.querySelector("#question-container");
 var startButton = document.getElementById("start-btn");
 
 var questionsArr = [
@@ -12,7 +12,7 @@ var questionsArr = [
             "20:4",
             "Single Rescuer does not deliver breaths"
             ],
-        correctAnswer: 1
+        correctAnswer: "30:2"
         },
         
         {
@@ -23,7 +23,7 @@ var questionsArr = [
             "Systolic blood pressure more than 90 mm Hg",
             "Respiratory Rate < 6/min or > 30/min"
             ],
-        correctAnswer: 2
+        correctAnswer: "Systolic blood pressure more than 90 mm Hg"
         },
 
         {
@@ -34,18 +34,48 @@ var questionsArr = [
             "Pulse checks every 2 min",
             "Short analyisis of heart rhythm"
             ],
-        correctAnswer: 1
+        correctAnswer:  "Unnecessarily moving patient"
         },
         {
-        question: "A monitoring device that gives information about both circulation and ventilation is",
+        question: "A monitoring device that gives information about both circulation and ventilation is:",
             answers: [
             "A colormetric capnograph",
             "Cardiac monitor",
             "Pulse Oximiter",
             "Reservoir Bag"
             ],
-        correctAnswer: 2
+        correctAnswer: "Pulse Oximiter"
         },
+        {
+        question: "What kind of stroke accounts for the majority of stroke?",
+            answers: [
+           "Intracerbral",
+            "Ischemic",
+            "Subarachnoid",
+            "Embolic"
+             ],
+        correctAnswer: "Ischemic"
+        },
+        {
+        question: "The T Wave on a cardiac monitor tracing is evidence of what change in polarization?",
+            answers: [
+            "Depolarization of the ventricles",
+            "Depolarization of the atria",
+            "Repolarization of the atria",
+            "Repolarization of the ventricles"
+            ],
+        correctAnswer: "Repolarization of the ventricles"
+        },
+        {
+        question: "Sinus bradycardia is defined by heartrate less than what?",
+            answers: [
+            "120 bpm",
+            "80 bpm",
+            "50 bpm",
+            "100 bpm"
+            ],
+        correctAnswer: "50 bpm"
+        }, 
     ];
 
 var qIndex = 0;
@@ -67,52 +97,58 @@ function setTime() {
 }
 
 //function to render the question and answers
-function renderQuestion(questionsArr) {
-    var answers; 
-     
-    //question rendering loop
-    //for (var i = 0; i < questionsArr.length; i++){
-        answers = [];
+function renderQuestion() {
+    //clears previous question
+    questionContainer.innerHTML="";
+        var answers; 
+        
+          //answers = [];
            
         //print question to page
            var  print = document.createElement("div");
            var  q = document.createTextNode(questionsArr[qIndex].question);
                
                 print.appendChild( q );
-                document.body.appendChild( q );
+                document.getElementById("question-container").appendChild( q );
 
                 //create series of answer buttons
            for (var n = 0; n < questionsArr[qIndex].answers.length; n++) {
             answers= questionsArr[qIndex].answers[n];  
-            
+                
                 var btn = document.createElement("button");
                 var j = document.createTextNode(answers);
                 var correctAnswer = questionsArr[qIndex].correctAnswer;
 
                 btn.setAttribute('class', 'btn btn-primary btn-block' );
                 btn.appendChild(j);
-                document.body.appendChild(btn);
+                document.getElementById("question-container").appendChild(btn);
                 
                 //Add click event to buttons that will decrease time for wrong answer
                 btn.addEventListener("click", function(event) {
-                    console.log(event);
-                    console.log(this);
-                var userAnswer = this.textContent;
-                var answerIndex = questionsArr.indexOf(userAnswer);
-
-                    // //if (answers[n] !== questionsArr[i].correctAnswer) {
-                        // secondsLeft-- ;
-                       
-                      
-                    // } else {
-                    //     renderQuestion(questionArr);
+                    //console.log(event);
+                    //console.log(this);
                     
-                    // }
+                var userAnswer = this.textContent;
+                //console.log(this)    
+                    if (userAnswer !== correctAnswer) {
+                        secondsLeft --;
+
+                    } else {
+                        qIndex ++;
+                       
+                        
+
+                        renderQuestion();
+                        //check out .removeChild to remove old buttons
+                    };
+                
+                    
                 });
             }
- //   }
+ 
 }
 
 
-renderQuestion(questionsArr);
-startButton.addEventListener("click", setTime());
+renderQuestion();
+setTime();
+
